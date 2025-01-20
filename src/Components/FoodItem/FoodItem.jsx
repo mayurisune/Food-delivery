27a0dc7ed5dc2e5bf87199b10f5f7f5409c2bdd8
Mayuri Sune <sunemayuri98@gmail.com>
2025-01-20 19:09:28 +0530

    added funtionality to product Count

--- src/Components/FoodItem/FoodItem.jsx
+++ src/Components/FoodItem/FoodItem.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
 import { food_list } from '../../assets/assets'
 const FoodItem = ({item}) => {
-
+const [itemcount, setItemCount]=useState(0);
   return (
     <div className="food-item">
         <div className="food-item-img-container">
         <img className='food-item-image' src={item.image} alt='' />
+        {
+          !itemcount
+          ?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt=''></img>
+          :<div className='food-item-counter'>
+           <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt=''></img>
+           <p>{itemcount}</p>
+           <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt=''></img>
+          </div>
+        }
         </div>
     <div className="food-item-info">
         <div className="food-item-name-rating">
